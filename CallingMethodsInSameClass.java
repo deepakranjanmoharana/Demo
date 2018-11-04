333955bfb7e2858e2e7de7f64eb09f0867bde90e
deepakranjanmoharana <dmoharana35@gmail.com>
2018-11-04 16:56:44 +0530

    added

--- CallingMethodsInSameClass.java
+++ CallingMethodsInSameClass.java
@@ -14,6 +14,7 @@ public class CallingMethodsInSameClass
 
 	public static void printOne() {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 	public static void printTwo() {
